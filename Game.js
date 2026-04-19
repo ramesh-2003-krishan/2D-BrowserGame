dcc5f9df93064d6b05fdf71c037595e49dacb9e9
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-04-19 18:23:16 +0530

    add circle for as game character

--- Game.js
+++ Game.js
@@ -1,3 +1,8 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+ctx.beginPath();
+ctx.arc(700, 150, 30, 0, 2 * Math.PI);
+ctx.fillStyle = 'blue';
+ctx.fill();
+ctx.stroke();
