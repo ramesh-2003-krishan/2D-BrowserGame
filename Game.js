b6e9bdd6757a765a46a3d9b687d197dd5b63e66d
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-04-20 18:34:50 +0530

    improve html and css

--- Game.js
+++ Game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 ctx.beginPath();
-ctx.arc(700, 150, 30, 0, 2 * Math.PI);
+ctx.arc(700, 150, 20, 0, 2 * Math.PI);
 ctx.fillStyle = 'blue';
 ctx.fill();
 ctx.stroke();
