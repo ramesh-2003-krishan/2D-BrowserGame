982c0c6c60159bb00d31357558ec029a792e9c96
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-04-21 08:31:14 +0530

    add ball movement

--- Game.js
+++ Game.js
@@ -1,8 +1,28 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+let x = canvas.width / 2;
+let y = canvas.height - 200;
+
+let dx = 2;
+let dy = -2;
+
+function draw() {
 ctx.beginPath();
-ctx.arc(700, 150, 20, 0, 2 * Math.PI);
+ctx.arc(x, y, 20, 0, 2 * Math.PI);
 ctx.fillStyle = 'blue';
 ctx.fill();
 ctx.stroke();
+}
+
+function moveBall() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+draw();
+
+x += dx;
+y += dy;
+
+requestAnimationFrame(moveBall);
+}
+
+moveBall();
